3ca49bb566b7f419ffa73c9fd947849d770c6ec8
dmdrk1414 <seungchan141414@gmail.com>
2023-11-26 04:00:01 +0900

    [Feat] Post 을 전체 탐색 기능 추가

--- assignment_04/src/main/java/org/example/repository/PostRepository.java
+++ assignment_04/src/main/java/org/example/repository/PostRepository.java
@@ -1,4 +1,19 @@
 package org.example.repository;
 
+import org.example.database.Database;
+import org.example.domain.Member;
+import org.example.domain.Post;
+
+import java.util.List;
+
 public class PostRepository {
+    private final Database database;
+
+    public PostRepository() {
+        this.database = new Database();
+    }
+
+    public List<Post> findAll() {
+        return database.findAllPost();
+    }
 }

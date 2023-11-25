42872bb96d267298e8b2deaa4a220b2c999a9716
dmdrk1414 <seungchan141414@gmail.com>
2023-11-26 03:59:03 +0900

    [Feat] Member 을 전체 탐색 기능 추가

--- assignment_04/src/main/java/org/example/repository/MemberRepository.java
+++ assignment_04/src/main/java/org/example/repository/MemberRepository.java
@@ -1,4 +1,19 @@
 package org.example.repository;
 
+import org.example.database.Database;
+import org.example.domain.Member;
+import org.example.domain.MemberPortfolio;
+
+import java.util.List;
+
 public class MemberRepository {
+    private final Database database;
+
+    public MemberRepository() {
+        this.database = new Database();
+    }
+
+    public List<Member> findAll() {
+        return database.findAllMember();
+    }
 }

df9e19aac64a75ea2771d1e2930626761e3c1091
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 20:06:59 +0900

    [Feat] TimeTable 도메인 생성

--- assignment_04/src/main/java/org/example/domain/TimeTable.java
+++ assignment_04/src/main/java/org/example/domain/TimeTable.java
@@ -1,4 +1,13 @@
 package org.example.domain;
 
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class TimeTable {
+    private final Integer id;
+    private final String image;
+    private final String applyDate;
 }

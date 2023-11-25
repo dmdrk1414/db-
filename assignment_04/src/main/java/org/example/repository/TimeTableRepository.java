b4577e15bb0278024041fafc5515cf2f95dcea8e
dmdrk1414 <seungchan141414@gmail.com>
2023-11-26 04:00:44 +0900

    [Feat] TimeTable 을 전체 탐색 기능 추가

--- assignment_04/src/main/java/org/example/repository/TimeTableRepository.java
+++ assignment_04/src/main/java/org/example/repository/TimeTableRepository.java
@@ -1,4 +1,19 @@
 package org.example.repository;
 
+import org.example.database.Database;
+import org.example.domain.Post;
+import org.example.domain.TimeTable;
+
+import java.util.List;
+
 public class TimeTableRepository {
+    private final Database database;
+
+    public TimeTableRepository() {
+        this.database = new Database();
+    }
+
+    public List<TimeTable> findAll() {
+        return database.findAllTimeTable();
+    }
 }

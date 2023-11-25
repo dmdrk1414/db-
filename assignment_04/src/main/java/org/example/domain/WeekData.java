fd017f82a15d3a84ebe023b3722a11cbad3152c0
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 20:10:13 +0900

    [Feat] WeekData 도메인 생성

--- assignment_04/src/main/java/org/example/domain/WeekData.java
+++ assignment_04/src/main/java/org/example/domain/WeekData.java
@@ -1,4 +1,18 @@
 package org.example.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class WeekData {
+    private final Boolean monday;
+    private final Boolean tuesday;
+    private final Boolean wednesday;
+    private final Boolean thursday;
+    private final Boolean friday;
 }

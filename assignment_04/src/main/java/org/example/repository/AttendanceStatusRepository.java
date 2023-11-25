3564a9315379f4873ef6ccfa9bfdcdb93ef7ab19
dmdrk1414 <seungchan141414@gmail.com>
2023-11-26 03:55:39 +0900

    [Feat] attendanceStatusRepository 을 전체 탐색 기능 추가

--- assignment_04/src/main/java/org/example/repository/AttendanceStatusRepository.java
+++ assignment_04/src/main/java/org/example/repository/AttendanceStatusRepository.java
@@ -1,4 +1,18 @@
 package org.example.repository;
 
+import org.example.database.Database;
+import org.example.domain.AttendanceStatus;
+
+import java.util.List;
+
 public class AttendanceStatusRepository {
+    private final Database database;
+
+    public AttendanceStatusRepository() {
+        this.database = new Database();
+    }
+
+    public List<AttendanceStatus> findAll() {
+        return database.findAllAttendanceStatus();
+    }
 }

85853a80da457c4d3deb4a51b5177254fc26e79e
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 21:12:27 +0900

    [Feat] 테이블 row 크기 관리 enum 생성

---
+++ assignment_04/src/main/java/org/example/constant/RowCount.java
@@ -0,0 +1,20 @@
+package org.example.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum RowCount {
+    ATTENDANCE_STATUS(5),
+    MANAGEMENT(4),
+    MEMBER(6),
+    MEMBER_PORTFOLIO(5),
+    POST(5),
+    TIME_TABLE(4),
+    WEEK_DATA(7);
+
+    private final Integer rowCount;
+
+    RowCount(Integer rowCount) {
+        this.rowCount = rowCount;
+    }
+}

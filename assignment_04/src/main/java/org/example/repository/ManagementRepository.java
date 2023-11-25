a6a646b62d9a4f17a640abcfe49157851d131b1c
dmdrk1414 <seungchan141414@gmail.com>
2023-11-26 03:56:53 +0900

    [Feat] ManagementRepository 을 전체 탐색 기능 추가

--- assignment_04/src/main/java/org/example/repository/ManagementRepository.java
+++ assignment_04/src/main/java/org/example/repository/ManagementRepository.java
@@ -1,4 +1,19 @@
 package org.example.repository;
 
+import org.example.database.Database;
+import org.example.domain.AttendanceStatus;
+import org.example.domain.Management;
+
+import java.util.List;
+
 public class ManagementRepository {
+    private final Database database;
+
+    public ManagementRepository() {
+        this.database = new Database();
+    }
+
+    public List<Management> findAll() {
+        return database.findAllManagement();
+    }
 }

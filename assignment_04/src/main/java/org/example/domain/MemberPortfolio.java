58733af3c63dcee36eebdd91aa424b6120a7ae06
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 20:01:41 +0900

    [Feat] MemberPortfolio 도메인 생성

--- assignment_04/src/main/java/org/example/domain/MemberPortfolio.java
+++ assignment_04/src/main/java/org/example/domain/MemberPortfolio.java
@@ -1,4 +1,22 @@
 package org.example.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class MemberPortfolio {
+    private final Integer id;
+    private final String motivationApplication;
+    private final String gpa;
+    private final String dream;
+
+    public MemberPortfolio(Integer id, String motivationApplication, String gpa, String dream) {
+        this.id = id;
+        this.motivationApplication = motivationApplication;
+        this.gpa = gpa;
+        this.dream = dream;
+    }
 }

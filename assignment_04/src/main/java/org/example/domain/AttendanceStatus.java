39cfddb1218e95596df26c857bc17a1862ea3256
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 19:56:05 +0900

    [Feat] AttendanceStatus 도메인 생성

--- assignment_04/src/main/java/org/example/domain/AttendanceStatus.java
+++ assignment_04/src/main/java/org/example/domain/AttendanceStatus.java
@@ -1,4 +1,23 @@
 package org.example.domain;
 
+import com.sun.jdi.IntegerType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class AttendanceStatus {
+    private final Integer id;
+    private final String status;
+    private final String attendanceTime;
+    private final String attendanceDate;
+
+    public AttendanceStatus(Integer id, String status, String attendanceTime, String attendanceDate) {
+        this.id = id;
+        this.status = status;
+        this.attendanceTime = attendanceTime;
+        this.attendanceDate = attendanceDate;
+    }
 }

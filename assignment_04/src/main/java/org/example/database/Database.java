a001241f08d4fb5c7bdc9aa36f52c415b766ef34
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 21:16:41 +0900

    [Feat] DBAttendanceStatus 전체 탐색 메서드 추가

--- assignment_04/src/main/java/org/example/database/Database.java
+++ assignment_04/src/main/java/org/example/database/Database.java
@@ -1,8 +1,20 @@
 package org.example.database;
 
 import lombok.Getter;
+import org.example.domain.AttendanceStatus;
+
+import java.sql.Statement;
+import java.util.List;
 
 @Getter
 public class Database {
+    private final Statement dbStatement;
 
+    public Database() {
+        this.dbStatement = new DBConnection().getStatement();
+    }
+
+    public List<AttendanceStatus> findAllAttendanceStatus() {
+        return new DBAttendanceStatus(dbStatement).findAll();
+    }
 }

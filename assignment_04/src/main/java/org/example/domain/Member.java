e93eb5797a7a5a39d8d6fa46304fd9ad5488cfb1
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 19:59:12 +0900

    [Feat] Member 도메인 생성

--- assignment_04/src/main/java/org/example/domain/Member.java
+++ assignment_04/src/main/java/org/example/domain/Member.java
@@ -1,4 +1,26 @@
 package org.example.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class Member {
+    private final Integer id;
+    private final String name;
+    private final String email;
+    private final String password;
+    private final String grade;
+    private final String phonNumber;
+
+    public Member(Integer id, String name, String email, String password, String grade, String phonNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.grade = grade;
+        this.phonNumber = phonNumber;
+    }
 }

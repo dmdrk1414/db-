a56b59d3aaca7322de6ded7302e5583bc1a4d593
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 20:02:48 +0900

    [Feat] Post 도메인 생성

--- assignment_04/src/main/java/org/example/domain/Post.java
+++ assignment_04/src/main/java/org/example/domain/Post.java
@@ -1,4 +1,22 @@
 package org.example.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class Post {
+    private final Integer id;
+    private final String title;
+    private final String content;
+    private final String postDate;
+
+    public Post(Integer id, String title, String content, String postDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.postDate = postDate;
+    }
 }

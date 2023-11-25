f51dabc6a02345162c68958aa78799f5b5663ef6
dmdrk1414 <seungchan141414@gmail.com>
2023-11-26 03:58:21 +0900

    [Feat] MemberPortfolio 을 전체 탐색 기능 추가

--- assignment_04/src/main/java/org/example/repository/MemberPortFolioRepository.java
+++ assignment_04/src/main/java/org/example/repository/MemberPortFolioRepository.java
@@ -1,4 +1,19 @@
 package org.example.repository;
 
+import org.example.database.Database;
+import org.example.domain.Management;
+import org.example.domain.MemberPortfolio;
+
+import java.util.List;
+
 public class MemberPortFolioRepository {
+    private final Database database;
+
+    public MemberPortFolioRepository() {
+        this.database = new Database();
+    }
+
+    public List<MemberPortfolio> findAll() {
+        return database.findAllMemberPortfolio();
+    }
 }

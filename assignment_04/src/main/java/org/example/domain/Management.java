99325caa36c5c4aee98f3358c6505aa5672944e9
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 19:57:30 +0900

    [Feat] Management 도메인 생성

--- assignment_04/src/main/java/org/example/domain/Management.java
+++ assignment_04/src/main/java/org/example/domain/Management.java
@@ -1,4 +1,20 @@
 package org.example.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class Management {
+    private final Integer id;
+    private final String date;
+    private final Integer vacationCount;
+
+    public Management(Integer id, String date, Integer vacationCount) {
+        this.id = id;
+        this.date = date;
+        this.vacationCount = vacationCount;
+    }
 }

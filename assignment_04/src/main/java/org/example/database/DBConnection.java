21e5965da47d47b93ef5f648f55d0c4041fbccdc
dmdrk1414 <seungchan141414@gmail.com>
2023-11-25 21:12:57 +0900

    [Refactor] 지역변수 추가

--- assignment_04/src/main/java/org/example/database/DBConnection.java
+++ assignment_04/src/main/java/org/example/database/DBConnection.java
@@ -1,14 +1,15 @@
 package org.example.database;
 
+import lombok.Getter;
+
 import java.sql.*;
 
+@Getter
 public class DBConnection {
     // 데이터베이스 연결을 위한 클래스
-
     private static final String ORACLE_JDBC_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
     private static final String ORACLE_DATABASE_ID = "C##assignment4";
     private static final String ORACLE_DATABASE_PASSWORD = "1234";
-    private Connection connection; // Connection 객체
     private Statement statement; // Statement 객체
 
     // DBConnectionTest 생성자
@@ -17,7 +18,8 @@ public class DBConnection {
             // Oracle 드라이버 로드
             Class.forName("oracle.jdbc.OracleDriver");
             // 데이터베이스 연결
-            connection = DriverManager.getConnection(ORACLE_JDBC_URL, ORACLE_DATABASE_ID, ORACLE_DATABASE_PASSWORD);
+            // Connection 객체
+            Connection connection = DriverManager.getConnection(ORACLE_JDBC_URL, ORACLE_DATABASE_ID, ORACLE_DATABASE_PASSWORD);
 
             statement = connection.createStatement(); // Statement 객체 생성
         } catch (ClassNotFoundException e) {
@@ -26,5 +28,4 @@ public class DBConnection {
             System.out.println("데이터베이스 연결 중 오류가 발생했습니다.\n" + e);
         }
     }
-    
 }

5838c2211aa7c8bd06e74506ee6be8bc01289198
dmdrk1414 <seungchan141414@gmail.com>
2023-11-27 15:05:35 +0900

    [Feat] View의 기본 프레임 설정

---
+++ assignment_04/src/main/java/org/example/Demo.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Demo extends JFrame {
+    public Demo() {
+        setTitle("데이터베이스");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(1500, 800);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Demo();
+    }
+}

5e4cf6dd0f2196014119841b57c89be3c59483fe
dmdrk1414 <seungchan141414@gmail.com>
2023-12-02 17:35:24 +0900

    [Feat] College entity 추가

---
+++ assignment_04/src/main/java/org/example/back/entity/College.java
@@ -0,0 +1,13 @@
+package org.example.back.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@ToString
+public class College {
+    private final String name;
+    private final String office;
+}

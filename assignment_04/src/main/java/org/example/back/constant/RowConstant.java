f2c56f004db7d8707efc533ed485d7f69673fe05
dmdrk1414 <seungchan141414@gmail.com>
2023-12-02 17:35:59 +0900

    [Feat] 테이블 속성 갯수 상수 관리 enum 추가

---
+++ assignment_04/src/main/java/org/example/back/constant/RowConstant.java
@@ -0,0 +1,22 @@
+package org.example.back.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum RowConstant {
+    COLLEGE(2),
+    COLLEGE_PHONE(3),
+    DEPARTMENT(5),
+    INSTRUCTOR(8),
+    INSTRUCTOR_RANK(2),
+    COURSE(7),
+    SECTION(10),
+    SECTION_TIME(3),
+    STUDENT(7);
+
+    private final Integer rowCount;
+
+    RowConstant(Integer rowCount) {
+        this.rowCount = rowCount;
+    }
+}

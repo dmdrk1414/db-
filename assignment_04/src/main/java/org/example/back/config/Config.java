321e335a920e353ccd869d391bdae4b8c9e48c8a
dmdrk1414 <seungchan141414@gmail.com>
2023-12-02 18:14:05 +0900

    [Feat]  department DB 속성을 entity 만들어주는 메서드 추가.

--- assignment_04/src/main/java/org/example/back/config/Config.java
+++ assignment_04/src/main/java/org/example/back/config/Config.java
@@ -2,6 +2,7 @@ package org.example.back.config;
 
 import org.example.back.entity.College;
 import org.example.back.entity.CollegePhone;
+import org.example.back.entity.Department;
 
 import java.util.List;
 
@@ -14,14 +15,24 @@ public class Config {
     }
 
     //    college_phone;
-    public static CollegePhone getCollegePhonFromRowArray(List<String> rows) {
+    public static CollegePhone getCollegePhoneFromRowArray(List<String> rows) {
         return CollegePhone.builder()
                 .id(rows.get(0))
                 .phone(rows.get(1))
                 .collegeName(rows.get(2))
                 .build();
     }
-//    department;
+
+    //    department;
+    public static Department getDepartmentFromRowArray(List<String> rows) {
+        return Department.builder()
+                .code(rows.get(0))
+                .name(rows.get(1))
+                .office(rows.get(2))
+                .phone(rows.get(3))
+                .collegeName(rows.get(4))
+                .build();
+    }
 //    instructor;
 //    instructor_rank;
 //    course;

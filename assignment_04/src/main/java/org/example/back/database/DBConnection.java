3d537474a378332148e48401bb2c10792afa6239
dmdrk1414 <seungchan141414@gmail.com>
2023-12-02 17:05:39 +0900

    [Feat] 데이터 베이스 연결 기능 추가

---
+++ assignment_04/src/main/java/org/example/back/database/DBConnection.java
@@ -0,0 +1,35 @@
+package org.example.back.database;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Getter
+public class DBConnection {
+    // 데이터베이스 연결을 위한 클래스
+    private static final String ORACLE_JDBC_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+    private static final String ORACLE_DATABASE_ID = "C##assignment4";
+    private static final String ORACLE_DATABASE_PASSWORD = "1234";
+    private Statement statement; // Statement 객체
+    private Connection connection;
+
+    // DBConnectionTest 생성자
+    public DBConnection() {
+        try {
+            // Oracle 드라이버 로드
+            Class.forName("oracle.jdbc.OracleDriver");
+            // 데이터베이스 연결
+            // Connection 객체
+            connection = DriverManager.getConnection(ORACLE_JDBC_URL, ORACLE_DATABASE_ID, ORACLE_DATABASE_PASSWORD);
+
+            statement = connection.createStatement(); // Statement 객체 생성
+        } catch (ClassNotFoundException e) {
+            System.out.println("해당 드라이버를 찾을 수 없습니다.\n" + e);
+        } catch (SQLException e) {
+            System.out.println("데이터베이스 연결 중 오류가 발생했습니다.\n" + e);
+        }
+    }
+}

16c37325319346f9e885a804a53dadf999adcc80
dmdrk1414 <seungchan141414@gmail.com>
2023-12-02 22:08:16 +0900

    [Feat] InstructorTable View 에 생성

--- assignment_04/src/main/java/org/example/front/Demo.java
+++ assignment_04/src/main/java/org/example/front/Demo.java
@@ -2,6 +2,7 @@ package org.example.front;
 
 import org.example.front.tableView.CollegePhoneTableView;
 import org.example.front.tableView.CollegeTableView;
+import org.example.front.tableView.InstructorTableView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,10 +14,12 @@ public class Demo extends JFrame {
         setTitle("데이터베이스");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // add. main View에 추가
         Container container = getContentPane();
         container.setLayout(new FlowLayout(FlowLayout.LEFT, MARGIN_WIDTH_VGAP, MARGIN_WIDTH_VGAP));
         container.add(new CollegeTableView());
         container.add(new CollegePhoneTableView());
+        container.add(new InstructorTableView());
 
         setSize(1500, 800);
         setVisible(true);
--- assignment_04/src/main/java/org/example/front/tableView/InstructorTableView.java
+++ assignment_04/src/main/java/org/example/front/tableView/InstructorTableView.java
@@ -1,7 +1,7 @@
 package org.example.front.tableView;
 
 import org.example.back.database.DatabaseManagement;
-import org.example.back.entity.College;
+import org.example.back.entity.Instructor;
 import org.example.front.constant.DBTable;
 
 import javax.swing.*;
@@ -10,15 +10,15 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.List;
 
-public class CollegeTableView extends JPanel {
+public class InstructorTableView extends JPanel {
 
-    private static final String TABLE_NAME = "college"; // add 1. 원하는 테이블 선택
+    private static final String TABLE_NAME = "instructor"; // add 1. 원하는 테이블 선택
     private static final Integer JSCROLL_PANE_HIGHT = 150;
     private final String[] tableColumnNames;
     private final DefaultTableModel mainTable;
     private final JTable table;
 
-    public CollegeTableView() {
+    public InstructorTableView() {
         tableColumnNames = new DBTable().get(TABLE_NAME);
         mainTable = new DefaultTableModel(tableColumnNames, 0);
         table = new JTable(mainTable);
@@ -31,7 +31,7 @@ public class CollegeTableView extends JPanel {
 
         // 테이블 크기
         table.setRowHeight(20);
-        table.getColumnModel().getColumn(1).setPreferredWidth(220); // add 3. 원하는 컬럼의 넓이 
+        table.getColumnModel().getColumn(1).setPreferredWidth(220); // add 3. 원하는 컬럼의 넓이
 
         // 라벨 글자 크기
         int fontSize = 20;
@@ -50,10 +50,10 @@ public class CollegeTableView extends JPanel {
 
     public void creatTable() {
         String[] columRow;
-        List<College> list = new DatabaseManagement().findAllCollege(); // add 4. 원하는 객체 선택
+        List<Instructor> list = new DatabaseManagement().findAllInstructor(); // add 4. 원하는 객체 선택
 
-        for (College college : list) { // add 5. 원하는 객체 선택
-            columRow = college.ofArray();
+        for (Instructor instructor : list) { // add 5. 원하는 객체 선택
+            columRow = instructor.ofArray();
             mainTable.addRow(columRow);
         }
     }

7234002fb26c6e74fb6b84f7a8bc0a31bed3aab1
dmdrk1414 <seungchan141414@gmail.com>
2023-12-02 18:16:36 +0900

    [Feat]  instructor DB 속성을 entity 만들어주는 메서드 추가.

--- assignment_04/src/main/java/org/example/back/config/Config.java
+++ assignment_04/src/main/java/org/example/back/config/Config.java
@@ -3,6 +3,7 @@ package org.example.back.config;
 import org.example.back.entity.College;
 import org.example.back.entity.CollegePhone;
 import org.example.back.entity.Department;
+import org.example.back.entity.Instructor;
 
 import java.util.List;
 
@@ -33,10 +34,28 @@ public class Config {
                 .collegeName(rows.get(4))
                 .build();
     }
-//    instructor;
-//    instructor_rank;
+
+    //    instructor;
+    public static Instructor getInstructorFromRowArray(List<String> rows) {
+        return Instructor.builder()
+                .id(rows.get(0))
+                .name(rows.get(1))
+                .office(rows.get(2))
+                .phone(rows.get(3))
+                .departmentName(rows.get(4))
+                .departmentCode(rows.get(5))
+                .collegeName(rows.get(6))
+                .chairStartDate(rows.get(7))
+                .build();
+    }
+
+    //    instructor_rank;
+
 //    course;
+
 //    section;
+
 //    section_time;
+
 //    student;
 }
